e8c79be24b73104437d8e6aa036de04b286754e1
Liam Egan <liam@wethecollective.com>
2021-11-17 08:38:27 -0800

    Adding initial setup for FX hash functionality

---
+++ src/index.js
@@ -0,0 +1,72 @@
+
+let fxrand;
+
+/**
+ * Initialises the library with a PRNG function. Most functions expect the PRNG to return 0-1 exclusive.
+ * @param prng - The PRNG function to use for subsequent calls
+ */
+export const FXInit = function(prng) {
+  if(typeof(prng) === 'function') fxrand = prng;
+}
+
+/**
+ * Checks for the existance of the FXHash PRNG and throws an error if it doesn't exist.
+ */
+const check = function() {
+  if (typeof fxrand !== "function")
+    throw new Error("fxhash has not been defined, did you call FXInit?");
+}
+
+/**
+ * Returns a random float between two numbers.
+ * ```
+ * FXRandomBetween(-10, 10); // -1.234576
+ * ```
+ * @param min - The minimum value
+ * @param max - The maximum value
+ */
+export const FXRandomBetween = (min, max) => {
+  check();
+  return min + (max - min) * fxrand();
+};
+
+/**
+ * Returns a random integer between two numbers.
+ * ```
+ * FXRandomBetween(-10, 10); // 2
+ * ```
+ * @param min - The minimum value
+ * @param max - The maximum value
+ */
+export const FXRandomIntBetween = (min, max) => {
+  check();
+  return Math.floor(FXRandomBetween(min, max));
+};
+
+/**
+ * Returns a vec2, expressed as an array, populated with random numbers 
+ * ```
+ * FXRandVec2(); // [.1234, .57351]
+ * ```
+ */
+export const FXRandVec2 = () => {
+  return [fxrand(), fxrand()];
+};
+/**
+ * Returns a vec3, expressed as an array, populated with random numbers 
+ * ```
+ * FXRandVec3(); // [.1234, .57351, .01234]
+ * ```
+ */
+export const FXRandVec3 = () => {
+  return [fxrand(), fxrand(), fxrand()];
+};
+/**
+ * Returns a vec4, expressed as an array, populated with random numbers 
+ * ```
+ * FXRandVec4(); // [.1234, .57351, .01234, .9634]
+ * ```
+ */
+export const FXRandVec4 = () => {
+  return [fxrand(), fxrand(), fxrand(), fxrand()];
+};
